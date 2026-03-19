a1fee55d8b45f214607be44e23d45e8e15fb2a9a
0401aysel <aysel0401@gmail.com>
2026-03-19 22:58:18 +0400

    Changed slider.js

--- slider.js
+++ slider.js
@@ -1,8 +1,6 @@
 let sliderLoad = document.querySelectorAll('.coffee-slider');
 let buttons = document.querySelectorAll('.product-types button');
-let addToBTN =document.querySelectorAll('.add-to-cart');
 let shop = document.querySelector('.cart-count');
-
 let cart ={};
 
 buttons.forEach(elem => {
@@ -21,35 +19,79 @@ buttons.forEach(elem => {
     });
 });
 
-addToBTN.forEach( button=>{
-    button.addEventListener('click',(e)=>{
-        let newElement = e.target.closest('.slider-item');
-        let id =newElement.dataset.id;
-        let price =newElement.querySelector('.price').innerText;
-        if(cart[id]){
-            cart[id].count++;
-        }else{
-            cart[id] = {
-                'id':id,
-                'count':1,
-                'price':price,
-            };
-        };
-        console.log(cart)
-
-        if(Object.keys(cart).length > 0){
-            let countProducts = 0;
-
-            for( let product of Object.values(cart)){
-                countProducts += product.count;
-            };
-
-            shop.innerHTML=countProducts;
-            shop.style.display='inline-block';
-        }else{
-            shop.innerHTML='0';
-            shop.style.display='none';
-        }
-    });
+fetch('./components/products.json')
+.then( res => res.json())
+.then(data=>{
+    renderSlide(data.slide1, '.slider1 .slider-items');
+    renderSlide(data.slide2, '.slider2 .slider-items');
+    renderSlide(data.slide3, '.slider3 .slider-items');
 });
+
+function renderSlide(data, selector){
+    const container = document.querySelector(selector);
+
+    container.innerHTML = '';
+
+    data.forEach(item =>{
+        const div = document.createElement('div');
+        div.className = 'slider-item';
+
+        div.innerHTML = `
+            <a href="/product.html" class="product-card-link">
+                <div class="radius-1">
+                    <img class="item-img" src="/images/${item.img}">
+                </div>
+                <p class="product-name">${item.name}</p>
+            </a>
+
+            <div class="rate">
+                <img src="/images/star.svg">
+                <p>${item.rate}</p>
+                <span>(${item.review} reviews)</span>
+            </div>
+
+            <p class="about-coffee">
+            Lorem ipsum dolor sit amet consectetur.
+            </p>
+
+            <div class="addToCart">
+                <div>
+                    <span class="price">${item.price}</span><span>$</span>
+                </div>
+                <button class="add-to-cart">
+                    <img src="/images/plus.svg">
+                </button>
+            </div>
+        `;
+
+        container.appendChild(div);
+
+        div.querySelector('.add-to-cart').addEventListener('click',()=>{
+            if(cart[item.id]){
+                cart[item.id].count++;
+            }else{
+                cart[item.id] = {
+                    'id':item.id,
+                    'count':1,
+                };
+            };
+
+            if(Object.keys(cart).length > 0){
+                let countProducts = 0;
+
+                for( let product of Object.values(cart)){
+                    countProducts += product.count;
+                };
+
+                shop.innerHTML=countProducts;
+                shop.style.display='inline-block';
+            }else{
+                shop.innerHTML='0';
+                shop.style.display='none';
+            }
+        });
+
+    });
+}
+
 
